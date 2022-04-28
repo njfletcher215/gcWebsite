ee805d884803dbaeae7fb77a6cc65423aff9140d
Nathan Fletcher <njfletcher215@gmail.com>
2022-04-28 06:09:52 +0000

    added better debugging

--- pull.php
+++ pull.php
@@ -3,7 +3,13 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-shell_exec( 'git reset -–hard HEAD && git pull' );
+    try { 
+        echo 'trying... ', shell_exec( 'cd /home/njfletcher215_gmail_com/gcWebsite 2>&1 && git reset --hard HEAD 2>&1 && git pull 2>&1' );
+        http_response_code(200);
+    } catch (Exception $e) {
+	echo 'Caught exception: ', $e->getMessage(), "\n";
+        http_response_code(500);
+    }
 }
 
 ?>
