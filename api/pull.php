4a62454896d0d1e7d8cfa71b0cd03e76bf58f13c
root <root@instance-1.us-central1-a.c.pelagic-magpie-306003.internal>
2022-04-28 07:38:57 +0000

    seperated frontend and backend

---
+++ api/pull.php
@@ -0,0 +1,17 @@
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+    try { 
+        echo 'trying...', shell_exec( 'cd /home/njfletcher215_gmail_com/gcWebsite 2>&1 && git reset --hard origin/master 2>&1 && git pull 2>&1' );
+        http_response_code(200);
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+        http_response_code(500);
+    }
+}
+
+?>
+
+Go Away!
