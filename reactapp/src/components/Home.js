1035e06ec9464d28f3bab09281283fba689648d2
Nathan Fletcher <Njfletcher215@gmail.com>
2022-04-28 12:36:21 -0500

    added home

---
+++ reactapp/src/components/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Greeting">
+        <p>
+          Hello!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
