1811bc9c1cfdfb7048ac09a791ea8e89d0846429
Nathan Fletcher <Njfletcher215@gmail.com>
2022-04-28 13:04:44 -0500

    changed response code

--- api/pull.php
+++ api/pull.php
@@ -4,8 +4,11 @@
 
 if ( $_POST['payload'] ) {
     try { 
-        echo 'trying...', shell_exec( 'cd /home/njfletcher215_gmail_com/gcWebsite 2>&1 && git reset --hard origin/master 2>&1 && git pull 2>&1' );
+        $response = exec( 'cd /home/njfletcher215_gmail_com/gcWebsite 2>&1 && git reset --hard origin/master 2>&1 && git pull 2>&1' );
         http_response_code(200);
+        if ($response == 0) {
+            http_response_code(500);
+        }
     } catch (Exception $e) {
         echo 'Caught exception: ',  $e->getMessage(), "\n";
         http_response_code(500);
