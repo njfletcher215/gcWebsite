4d84ef8195d9ddc0bfd4cca19b3ca123faf1b50d
Nathan Fletcher <Njfletcher215@gmail.com>
2022-05-05 01:31:36 -0500

    added light styling to about me

--- reactapp/src/components/pages/Home.js
+++ reactapp/src/components/pages/Home.js
@@ -1,9 +1,9 @@
 import '../../styles/Home.css';
 import Image from '../Image';
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <div className="Home">
+    <div className={`home ${props.className || ''}`}>
       <Image className="home-image" link="/AboutMe" hoverText="View Save_As Campaign" source='/work/FNAR332/SaveAsCampain/final/Mockups/Logo_Mockup_2300x1500@72.png'/>
     </div>
   );
